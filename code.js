d04c3f5637f9e39982777186ee94d27b8c044ddb
malam1998 <malam.199810@gmail.com>
2025-09-01 23:22:17 -0400

    first commit

---
+++ code.js
@@ -0,0 +1,49 @@
+const heartbtns = document.getElementsByClassName("heart-icon");
+let heartCount = 0;
+
+callHistory = [];
+// get element by ID function
+function getElement(id) {
+  const element = document.getElementById(id);
+  return element;
+}
+
+// heart count event
+for (const btns of heartbtns) {
+  btns.addEventListener("click", function () {
+    const heart = getElement("heart-no");
+    heartCount++;
+    heart.innerText = heartCount;
+    btns.classList.add("hover:bg-red-500");
+  });
+}
+
+// calling section
+const callIcons = document.getElementsByClassName("call-btn");
+for (const callIcon of callIcons) {
+  callIcon.addEventListener("click", function () {
+    const card = callIcon.closest(".card");
+    const title = card.querySelector(".card-title").innerText;
+    const number = card.querySelector(".card-number").innerText;
+
+    const coins = parseInt(document.getElementById("coin-value").innerText);
+    const totalcoin = coins - 20;
+
+    if (totalcoin < 0) {
+      alert("Not enough coins");
+      return;
+    }
+    document.getElementById("coin-value").innerText = totalcoin;
+
+    alert(`📞 Calling ${title} ${number}`);
+
+    const history = {
+      name: title,
+      number: number,
+      date: new Date().toLocaleString(),
+    };
+
+    callHistory.push(history);
+    // console.log(callHistory);
+  });
+}
